f8f64608cc6a5a7b0e4b749e56c1c1a3057df166
descatalogao <aitorolano7@gmail.com>
2024-06-24 09:09:12 +0200

    first

---
+++ components/Divider/Divider.js
@@ -0,0 +1,3 @@
+export const Divider = () => `
+<span class="divider"></span>
+`;
---
+++ main.js
@@ -0,0 +1,21 @@
+import { changeTheme } from "./components/Navbar/Navbar.js";
+import { linkPage } from "./utils/linkPage.js";
+import { Navbar } from "./components/Navbar/Navbar.js";
+import { Footer } from "./components/Footer/Footer.js";
+import { Home } from "./pages/Home/Home.js";
+import { Projects } from "./pages/Projects/Projects.js";
+import { Divider } from "./components/Divider/Divider.js";
+
+const header = document.querySelector("header");
+header.innerHTML = Navbar();
+const footer = document.querySelector("footer");
+footer.innerHTML = Footer();
+
+linkPage("#homelink", Home);
+linkPage("#projectslink", Projects);
+
+Home();
+
+changeTheme();
+
+footer.insertAdjacentHTML("beforebegin", Divider());
