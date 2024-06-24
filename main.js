98bf09cb67aed81af19385c300fcb42d6d40f5a9
descatalogao <aitorolano7@gmail.com>
2024-06-24 20:04:09 +0200

    import.css metido en main

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-import { changeTheme } from "./components/Navbar/Navbar.js";
-import { linkPage } from "./utils/linkPage.js";
-import { Navbar } from "./components/Navbar/Navbar.js";
-import { Footer } from "./components/Footer/Footer.js";
-import { Home } from "./pages/Home/Home.js";
-import { Projects } from "./pages/Projects/Projects.js";
-import { Divider } from "./components/Divider/Divider.js";
+import './style.css' 
+import { changeTheme } from "../my-portfolio/components/Navbar/Navbar.js";
+import { linkPage } from "../my-portfolio/utils/linkPage.js";
+import { Navbar } from "../my-portfolio/components/Navbar/Navbar.js";
+import { Footer } from "../my-portfolio/components/Footer/Footer.js";
+import { Home } from "../my-portfolio/pages/Home/Home.js";
+import { Projects } from "../my-portfolio/pages/Projects/Projects.js";
+import { Divider } from "../my-portfolio/components/Divider/Divider.js";
 
 const header = document.querySelector("header");
 header.innerHTML = Navbar();
